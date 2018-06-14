5ca0687a601e066841502cf2db790820fe4fe548
Emily Howing <emilyhowing@Emilys-MacBook-Pro-4.local>
2018-06-14 12:52:42 -0500

    ping api completed

--- home.py
+++ home.py
@@ -1,1 +1,21 @@
+import requests
+import urllib
+import json
 
+
+
+appId = "9f58709e"
+key = "9eeb6b82d09c485293c9a9e1b0dce5d2"
+query = "chicken"
+
+#URL = 'https://api.edamam.com/search?q' + query + '=&app_id=' + appId + '&app_key=' + key + '&cal=500'
+URL = "https://api.edamam.com/search?q=chicken&app_id=9f58709e&app_key=9eeb6b82d09c485293c9a9e1b0dce5d2&from=0&to=3&cal=500"
+r = requests.get(url=URL)
+
+#data = r.json()
+
+#print("data: " + data.dumps());
+response = urllib.request.urlopen(URL)
+str_response = response.read().decode('utf-8')
+obj = json.loads(str_response)
+print(obj)
