3f62656314bf823752654ac79e5512fbdf13fb69
Emily Howing <emilyhowing@Emilys-MacBook-Pro-4.local>
2018-06-14 13:18:19 -0500

    html code added

--- home.py
+++ home.py
@@ -7,7 +7,6 @@ key = "9eeb6b82d09c485293c9a9e1b0dce5d2"
 query = "chicken"
 
 URL = 'https://api.edamam.com/search?q=' + query + '&app_id=' + appId + '&app_key=' + key + '&from=0&to=3&cal=500'
-#URL = "https://api.edamam.com/search?q=chicken&app_id=9f58709e&app_key=9eeb6b82d09c485293c9a9e1b0dce5d2&from=0&to=3&cal=500"
 r = requests.get(url=URL)
 
 response = urllib.request.urlopen(URL)
